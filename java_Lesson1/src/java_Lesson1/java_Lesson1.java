1ca88facd453a5fe19e856759c6e6239d1d62b56
Liviu Coman <liviu.coman10@gmail.com>
2013-08-20 13:32:16 +0300

    First commint to the First Tutorial

---
+++ java_Lesson1/src/java_Lesson1/java_Lesson1.java
@@ -0,0 +1,10 @@
+package java_Lesson1;
+
+public class java_Lesson1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
