c00110ed948599be0d126d6456d46fdfd6ecbf9d
Liviu Coman <liviu.coman10@gmail.com>
2013-08-20 13:35:30 +0300

    Cpp Commit 1 for the first Cpp project

---
+++ cpp_Lesson1/src/cpp_Lesson1.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : cpp_Lesson1.cpp
+// Author      : Liviu Coman
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
